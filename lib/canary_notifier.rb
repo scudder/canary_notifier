038f46f281d0e4b814770535ac98a7176216a2b2
Henry Scudder <scudder@canaryhq.com>
2011-11-07 21:39:29 -0600

    simplification to new format

--- lib/canary_notifier.rb
+++ lib/canary_notifier.rb
@@ -9,51 +9,25 @@ class CanaryNotifier
     @app.call(env)
   rescue Exception => e
     url = 'http://stark:abc123@bugdrop.bugdrop.io/notify'
-    (file_name, line_number, method) = parse_backtrace_line(find_our_backtrace_line(e.backtrace))
 
     data = {}
-    data["app_name"] = @options[:app_name] || "No Name"
-    data["bug_name"] = "#{e.class.name}: #{e.to_s}"
-    data["bug_class"] = e.class.name
-    data["file_name"] = file_name
-    data["line_number"] = line_number.to_i
-    data["bug_method"] = method.split.last
-    data["language"] = 'ruby'
-    data["backtrace"] = e.backtrace.join("\n")
+    data["app"] = @options[:app_name] || "No Name"
+    data["title"] = "#{e.class.name}: #{e.to_s}"
 
-    lines = File.readlines(file_name)
-    start_line = line_number.to_i - 4
-    start_line = 0 if start_line < 0
-    end_line = line_number.to_i+2
-    end_line = lines.size - 1 if end_line > lines.size - 1
+    data[:sections] = []
 
-    data["code_sample"] = lines[start_line..end_line].join
+    backtrace = {}
+    backtrace[:title] = 'Backtrace'
+    backtrace[:body] = e.backtrace.join("\n")
 
-    data["code_sample_line_start"] = line_number.to_i - 3
-    data["code_sample_line_start"] = 1 if data["code_sample_line_start"] < 1
+    data[:sections] << backtrace
+
+    env = {}
+    env[:title] = 'Environment'
+    env[:body] = ENV.to_a
+
     RestClient.post url,  data.to_json
  
     raise e
-  end
-
-  def parse_backtrace_line(line)
-    parts = line.split(':')
-   
-    # the backtrace will always parse funny on Windows due to paths containing things like 
-    # 'c:\ruby\etc...'
-    if parts.size == 4
-      file = parts[0..1].join(':')
-      line_num = parts[2]
-      method = parts[3]
-      [file, line_num, method] 
-    else
-      parts
-    end
-  end
-
-  def find_our_backtrace_line(backtrace)
-    backtrace.each do |l|
-      return l if !l.match(/gems/)
-    end
   end
 end
